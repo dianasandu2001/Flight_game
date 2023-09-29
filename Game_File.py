24f53ab3cd7113287bdcff1bf8211ccac11ea233
Diana <sandu.diana2001@gmail.com>
2023-09-29 11:14:06 +0300

    First commit

---
+++ Game_File.py
@@ -0,0 +1,13 @@
+#Connection to DataBase
+
+#Variable
+fuel = 20_000
+totalDistance = 0
+distanceTraveled = 0
+countryTravaled = 0
+countryGoal = 5
+
+#Functions
+
+#Game code
+While countryTravaled < countryGoal:
