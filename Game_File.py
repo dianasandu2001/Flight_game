f50f7a3b7d57176256ec4154d4dabf91ad880fe2
Diana <sandu.diana2001@gmail.com>
2023-09-29 12:06:26 +0300

    start of game code again

--- Game_File.py
+++ Game_File.py
@@ -13,10 +13,15 @@ connection = mysql.connector.connect(
 """
 #Variable
 fuel = 20_000
+fuelUsage = 0
 totalDistance = 0
 distanceTraveled = 0
 countryTraveled = 0
 countryGoal = 5
+
+#Lists
+    Questions = []
+    Answers = []
 
 #Functions
 def distanceairport():
@@ -36,22 +41,37 @@ def distanceairport():
             location.append(lat)
             location.append(long)
         distance = geodesic((location[0],location[1]),(location[2],location[3]))
-        print(f"Distance between these two airport is: {distance}")
+        return distance
 
 # Function to convert km to fuel:
 def convertkmtofuel(distance):
     fuelUsage = distance*12
     return fuelUsage
 # Function to display variables after:
-def updateVar():
+def updateVar(fuel, totalDistance):
     fuel = fuel - fuelUsage
-    print(fuel)
+    print(f"Your remaining fuel is: {fuel}l")
+    distanceTraveled = distanceairport()
     totalDistance = totalDistance + distanceTraveled
-    print(totalDistance)
-    print(countryTraveled)
+    print(f"You have travelled: {totalDistance}km")
+    print(f"Countries travelled to: {countryTraveled}")
 
 #Game code
+userName = input("Please enter your name: ")
+print(f"Hello {userName}, welcome to our flight game!\nYour goal is to visit 5 different countries without running out of fuel.\nYou have been given 20 000 liters of fuel, and will be given the oportunity to gain more later by answering trivia qustions.\nHave fun and good luck! :)")
+updateVar(fuel, totalDistance)
+countryDestination = input("Which country would you like to go to (in Europe)?: ")
+#filter out 3 choices of airports for the country
+airportDestination = input(f"{}this is the filtered\nWhich airpot would you like to visit in {countryDestination}: {}")
 while countryTraveled < countryGoal:
-    userName = input("Please enter your name: ")
-    print(f"Hello {userName}, welcome to our flight game!\nYour goal is to visit 5 different countries without running out of fuel.\nYou have been given 20 000 liters of fuel, and will be given the oportunity to gain more later by answering trivia qustions.\nHave fun and good luck! :)")
-    updateVar()
+    #ask the question
+        if answer is in list:
+            fuel = fuel + 5000
+            print("Answer is correct, 5000l of fuel awarded")
+        else:
+            print("Answer is wrong, no fuel awarded")
+    updateVar(fuel,totalDistance)
+    countryDestination = input("Which country would you like to go to next?")
+    #filter out 3 choices of airports for the country
+    airportDestination = input(f"{}this is the filtered\nWhich airpot would you like to visit in {countryDestination}: {}")
+    break
