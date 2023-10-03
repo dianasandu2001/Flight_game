9ff2c5640f5d5cf90c2b4eb06aa5973fd67a2629
diana <sandu.diana2001@gmail.com>
2023-10-03 15:05:46 +0300

    Questions in progress 2

--- testing the questions.py
+++ testing the questions.py
@@ -1,11 +1,13 @@
-Questions = ["Romania is the largest salt mine in Europe", ]
-Answers = ["true", ]
+import random
+
+Questions = ["Romania is the largest salt mine in Europe", "Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9", "Q10"]
+Answers = ["true", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a8", "a9", "a10"]
 
 number = random.randint(0, (len(Questions) - 1))
-print(random.choice(Questions))
-answer = lower.input("Is this true or false: ")
-if answer in Answers:
-    fuel = fuel + 5000
+print(Questions[number])
+answer = input("Is this true or false: ").lower()
+if answer == Answers[number]:
+    #fuel = fuel + 5000
     print("Answer is correct, 5000l of fuel awarded")
 else:
     print("Answer is wrong, no fuel awarded")
