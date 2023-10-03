a92a39e11ffcae6e4c41aff554b3a6c8954a284e
diana <sandu.diana2001@gmail.com>
2023-10-03 18:08:06 +0300

    Questionans answers lists

--- Game_File.py
+++ Game_File.py
@@ -22,8 +22,8 @@ countryTraveled = 0
 countryGoal = 5
 
 #Lists
-    Questions = ["Romania is the largest salt mine in Europe", ]
-    Answers = ["true", ]
+    Questions = ["Romania has the largest salt mine in Europe.", "Iceland doesn’t have mosquitos.", "Norway knighted a penguin. More than once.", "Wales has more sheep than people.", "It is illegal to take a picture of the Eiffel Tower.", "More than 200 Languages are spoken throughout Europe.", "The Stonehenge is the most visited attraction in Europe.", "French fries were Invented in Belgium.", "No Cappuccino After 11 Am in Italy. (social stigma)", "Spain produces 60% of Europe’s cheese.", "Of the British Museum's Collection, Only 1% Is on Display.", "It Is Illegal to Name Your Pig Napoleon in France.", "The Grand Canal in Venice is 50% wine", "Finland has the second most amount of lakes in Europe", "More Chocolate Is Bought at Brussels Airport Than Any Other Place in the World.", "Germany's Famous Oktoberfest Is Actually in November.", "80% of Greece is made up of mountains.", "Sweden is home of the first Christmas tree.", "The Danish language has no word for 'please'.", "LEGO was invented by a Russian."]
+    Answers = ["true", "true", "true", "true", "false", "true", "false", "true", "true", "false", "true", "true", "false", "false", "true", "false", "true", "false", "true", "false"]
 
 #Functions
 def distanceairport():
@@ -77,15 +77,17 @@ countryDestination = input("Which country would you like to go to (in Europe)?: 
 #filter out 3 choices of airports for the country
 airportDestination = input(f"{}this is the filtered\nWhich airpot would you like to visit in {countryDestination}: {}")
 while countryTraveled < countryGoal:
+
     #ask the question
-    number = random.randint(0, (len(Questions)-1))
-    print(random.choice(Questions))
-    answer = lower.input("Is this true or false: ")
-        if answer in Answers:
-            fuel = fuel + 5000
-            print("Answer is correct, 5000l of fuel awarded")
-        else:
-            print("Answer is wrong, no fuel awarded")
+    number = random.randint(0, (len(Questions) - 1))
+    print(Questions[number])
+    answer = input("Is this true or false: ").lower()
+    if answer == Answers[number]:
+        # fuel = fuel + 5000
+        print("Answer is correct, 5000l of fuel awarded.")
+    else:
+        print("Answer is wrong, no fuel awarded.")
+
     updateVar(fuel,totalDistance)
     countryDestination = input("Which country would you like to go to next?")
     #filter out 3 choices of airports for the country
